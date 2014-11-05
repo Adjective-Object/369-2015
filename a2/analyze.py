1e6550db62264e79bb66c2bb0d1b2deb98ad6cd3
c3huangk <c3huangk@187f79c2-63e7-4bff-9525-382eaad7e6cc>
2014-11-05 00:07:59 +0000

    added table formatting and comments to the analysis script
    git-svn-id: https://markus.cdf.toronto.edu/svn/csc369-2014-09/group_0061@22 187f79c2-63e7-4bff-9525-382eaad7e6cc

--- a2/analyze.py
+++ a2/analyze.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 import sys
 
-def readMarkerFile(argv):
+
+def readMarkerFile(markerfile):
     """
     reads the contents of a given marker file if one is specified,
     otherwise, it reads the contents of ./marker.
@@ -11,71 +11,156 @@ def readMarkerFile(argv):
     outputs the addresses of the beginning and end of the relavant
     areas of the trace (marer_start, marker_end)
     """
-
-    mf = mf = sys.argv[2] if (len(argv) == 3) else "./marker"
-
-    with open(mf, 'r') as f:
+    with open(markerfile, 'r') as f:
         m_start, m_end = f.readline().split(" ")
 
     return m_start[2:], m_end[2:]
 
-def isDataPage(line):
-    return (line.startswith(" S") or
-            line.startswith(" L") or
-            line.startswith(" M"))
 
-
-def analyzeTrace(argv):
+def dropComments(f, line):
     """
-    determines the number of code and data pages from a valgrind lackey trace
+    Reads a series of lines from a file while they begin with the string "=="
+    returns the first line not beginning with "=="
 
+    Used to drop the section of comments from the beginning of the file.
     """
-    # read marker from file
-    marker_start, marker_end = readMarkerFile(argv)
-    #print marker_start, marker_end
+    while line.startswith("=="):
+        line = f.readline();
+    return line
 
+
+def readBody(f, line, marker_start, marker_end):
+    """
+    (File, String, Int, Int) -> (Int, Int, Int, Int)
+
+    Reads the body of a valgrind lackey trace 
+    (i.e. the section between the ==comments== at the head and tail of the file)
+
+    Called by analyzeTrace, (see analyzeTrace for return values)
+    """
     # initialize counter variables
     codepages, datapages = [], []
     outsideCount = 0
     insideCount = 0
+    inside = False
 
-    # open file and process it line by line
-    with open(argv[1], 'r') as f:
-        reading = False
-        for line in f:
-            #ignore comment lines at beginning
-            if not line.startswith("=="):
-                # only count when between mem markers
-                if not reading and line.split(" ")[-1].startswith(marker_start):
-                    reading = True
-                elif line.split(" ")[-1].startswith(marker_end):
-                    reading = False
-                
-                if reading:
-                    address = line.split(" ")[-1]# find the block with the addr
-                    pagenumber = address.split(",")[0][0:-3]# cut out the offset
-                    #print address, pagenumber
+    while not line.startswith("=="):
+        # only count when between mem markers
+        # find the block with the addr
+        address = line.split(" ")[-1].split(",")[0]
+        pagenumber = address[0:-3]
 
-                    if isDataPage(line):
-                        if(pagenumber not in datapages):
-                            datapages.append(pagenumber)
-                        insideCount += 1
+        # exit the 'inside' section if the parser encounters the marker_end addr
+        if address == marker_end:
+            inside = False
 
-                    elif pagenumber not in codepages:
-                        codepages.append(pagenumber)
-                
-                elif isDataPage(line):
-                    outsideCount += 1
+        # count the number of S/L/R that happen inside/outside of the main block
+        if not line.startswith("I"):
+            if inside:
+                insideCount += 1
+            else:
+                outsideCount += 1
 
-    #print codepages, datapages
-    return len(codepages), len(datapages), outsideCount, insideCount
+        # manage the contents of codepages properly 
+        if line.startswith("I"):
+            if pagenumber not in codepages:
+                codepages.append(pagenumber)
+
+        else:
+            if pagenumber not in datapages:
+                datapages.append(pagenumber)
+
+
+        # enter the 'inside' section if 
+        # the parser encounters the marker_start addr
+        if address == marker_start:
+            inside = True
+
+        # update line
+        line = f.readline()
+
+    return len(codepages), len(datapages), insideCount, outsideCount
+
+
+def analyzeTrace(tracefile, markerfile):
+    """
+    (String, String) -> (Int, Int, Int, Int)
+
+    reads a valgrind lackey trace and a marker file and determines:
+
+    I:            the number of pages from which instructions are read
+    S/L/M:        the number of pages from which data is set/loaded/moved
+    insideCount:  the number of instrutions between marker_start and marker_end
+    outsideCount: the number of instructions not between 
+                    marker_start and marker_end
+
+    """
+    # read marker from file
+    marker_start, marker_end = readMarkerFile(markerfile)
+    #print marker_start, marker_end
+
+    # open file and read the first line
+    f = open(tracefile, 'r')
+    line = f.readline()
+
+    # ignore the comment lines at beginning
+    line = dropComments(f, line)
+
+    # read the body of the trace
+    traceData = readBody(f, line, marker_start, marker_end)
+
+    # close the file
+    f.close()
+
+    return traceData 
+
+
+def printTable(table):
+    """
+    ([[String]]) -> None
+
+    prints the contents of a table, defined in a mxn 2 dimensional list, indexed
+    row first.
+
+    the first row is assumed to be headers, and the list is assumed to have
+    conscistent dimensions (i.e. all rows of equal length).
+    """
+    # calculate columns widths
+    cwidths = [0] * len(table[0])
+    for row in table:
+        for i in range(len(row)):
+            cwidths[i] = max(len(row[i]), cwidths[i])
+    #
+    rowlen = sum([x+3 for x in cwidths])+ 1
+
+    print " " + (rowlen-2) * "-" + " "
+
+
+    for ind in range(len(table)):
+        if(ind == 1):
+           print "|" + (rowlen-2) * "-" + "|"
+        row = table[ind]
+        row = [row[i] + " " * 
+            (cwidths[i] - len(row[i])) for i in range(len(row))]
+        print "| "+" | ".join(row)+" |"
+
+    print " " + (rowlen-2) * "-" + " "
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 and len(sys.argv) != 3:
-        print( "analyze.py: no tracefile specified.\n"
-               "usage is \"analyze.py <tracefile> <markerfile>\"\n"
-               "where <markerfile> defaults to \"./marker\" if not provided"
+    if len(sys.argv) %2  != 1:
+        print( "analyze.py: improper args.\n"
+               "usage is \"analyze.py <trace1> <marker1> "
+               "<trace2> <marker2> ...\"\n"
              )
-    else:
-        print analyzeTrace(sys.argv)
+    #divide the arguments into (tracefile, markerfile) pairs
+    pairs = [(sys.argv[i], sys.argv[i+1]) for i in range(1,len(sys.argv),2)]
+
+    # Initialize the headers of the table
+    table = [["Tracefile", "Markerfile", "I", "S/L/R", "Inside", "Outside"]]
+    for trace, marker in pairs:
+        ptrace = trace.split("/")[-1]
+        pmarker = marker.split("/")[-1]
+        table.append( [ptrace, pmarker]+ map(str, analyzeTrace(trace, marker)))
+
+    printTable(table)
